7790eddb081c1b7de71bab67484998f8264cb324
kevinJV <163199@ids.upchias.edu.mx>
2019-07-20 13:37:09 -0500

    test created

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('get /', () => {
+    it('respond with hello there', (done) => {
+        request(app).get('/').expect('Hello there', done);
+    });
+});
