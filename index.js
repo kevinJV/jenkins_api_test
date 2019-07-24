b14347344bd5d8cd7da25c093bbc31af6f0cdfda
kevinJV <163199@ids.upchias.edu.mx>
2019-07-23 19:20:47 -0500

    Post

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res)=>{
     res.send('Hello there; General Kenobi')
 });
+
+app.post('/', (req, res)=>{
+    const { num1, num2 } = req.body;
+    const sum  = num1 + num2;
+    console.log(sum)
+    res.send('suma: ' + sum)
+})
 
 app.listen(process.env.PORT || 3000);
 console.log("Server started")
--- test/test.js
+++ test/test.js
@@ -3,6 +3,19 @@ const app = require('../index');
 
 describe('get /', () => {
     it('respond with hello there', (done) => {
-        request(app).get('/').expect('Hello there; General Kenobi', done);
+        request(app).get('/')
+        .expect('Hello there; General Kenobi', done);
+    });
+});
+
+describe('post /', () => {
+    body = {
+        "num1": 1,
+        "num2": 3
+    }
+    it('respond with hello there', (done) => {
+        request(app).post('/')
+        .send(body)
+        .expect('suma: 4', done);
     });
 });
